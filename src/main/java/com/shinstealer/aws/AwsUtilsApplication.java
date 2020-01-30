7b10d733d3ddb0178b796c3a1aa4cf34c0341ee9
shinstealer <39997731+Shinstealer@users.noreply.github.com>
2020-01-30 11:03:52 +0900

    first init
    first init

---
+++ src/main/java/com/shinstealer/aws/AwsUtilsApplication.java
@@ -0,0 +1,16 @@
+package com.shinstealer.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+public class AwsUtilsApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AwsUtilsApplication.class, args);
+		
+	}
+
+}
